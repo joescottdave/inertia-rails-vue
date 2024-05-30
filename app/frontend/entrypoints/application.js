9729621d723d9be21b669c5d92c24cee7f7b4cd0
joescottdave <joseph.davies@outlook.com>
2024-05-30 14:47:40 +0100

    add first inertia route

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -1,28 +1,22 @@
-// To see this message, add the following to the `<head>` section in your
-// views/layouts/application.html.erb
-//
-//    <%= vite_client_tag %>
-//    <%= vite_javascript_tag 'application' %>
-console.log('Vite ⚡️ Rails')
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
 
-// If using a TypeScript entrypoint file:
-//     <%= vite_typescript_tag 'application' %>
-//
-// If you want to use .jsx or .tsx, add the extension:
-//     <%= vite_javascript_tag 'application.jsx' %>
+createInertiaApp({
+  resolve: name => {
+    const pages = import.meta.glob('../Pages/**/*.vue', { eager: true })
+    const component =  pages[`../Pages/${name}.vue`]
 
-console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify.app/guide/rails')
+    if (component == null) {
+      throw new Error(
+        `Unknown page ${name}. Is it located under Pages with a .vue extension?`,
+      )
+    }
 
-// Example: Load Rails libraries in Vite.
-//
-// import * as Turbo from '@hotwired/turbo'
-// Turbo.start()
-//
-// import ActiveStorage from '@rails/activestorage'
-// ActiveStorage.start()
-//
-// // Import all channels.
-// const channels = import.meta.globEager('./**/*_channel.js')
-
-// Example: Import a stylesheet in app/frontend/index.css
-// import '~/index.css'
+    return component
+  },
+  setup({ el, App, props, plugin}) {
+    createApp({ render: () => h(App, props)})
+      .use(plugin)
+      .mount(el)
+  }
+})
